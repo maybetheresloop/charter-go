d9e41ba6f7b675b5f3015273142b807fc8390ecc
maybetheresloop <maybetheresloop@gmail.com>
2020-01-02 17:34:25 -0800

    Add CSV backend implementation.

--- passwd/engine/csv/main.go
+++ passwd/engine/csv/main.go
@@ -1,6 +1,11 @@
 package csv
 
 import (
+	"encoding/csv"
+	"errors"
+	"io"
+	"os"
+
 	"github.com/maybetheresloop/charter-go/passwd"
 )
 
@@ -8,15 +13,40 @@ type connector map[string]string
 type driver struct{}
 
 var (
-	ctx passwd.Driver = driver{}
+	drv passwd.Driver = driver{}
 )
 
 func init() {
-	passwd.Register("csv", ctx)
+	passwd.Register("csv", drv)
 }
 
-func (d driver) OpenConnector(dataSourceName string) (passwd.Connector, error) {
+func (drv driver) OpenConnector(dataSourceName string) (passwd.Connector, error) {
+	f, err := os.Open(dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
 	c := make(connector)
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		if len(record) != 2 {
+			return nil, errors.New("malformed record")
+		}
+
+		c[record[0]] = record[1]
+	}
+
 	return c, nil
 }
 
